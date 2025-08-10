feb7d3c1c4e48073dcdbafdc64cc24b27332c942
emo-mruczek <krokcia1@gmail.com>
2025-08-10 17:04:18 +0200

    feat: getting list of tracks

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ async fn main() {
     
     let creds = Credentials::from_env().unwrap();
 
-    let oauth = OAuth::from_env(scopes!("user-read-playback-state")).unwrap();
+    let oauth = OAuth::from_env(scopes!("user-library-read")).unwrap();
 
     let mut spotify = AuthCodePkceSpotify::new(creds.clone(), oauth.clone());
 
     let url = spotify.get_authorize_url(None).unwrap();
     spotify.prompt_for_token(&url).await.unwrap();
 
-    let history = spotify.current_playback(None, None::<Vec<_>>).await;
-    println!("Response: {:#?}", history);
+    let tracks = spotify.current_user_saved_tracks_manual(None, None, None).await.unwrap();
+
+    println!("Response: {:#?}", tracks);
 }
